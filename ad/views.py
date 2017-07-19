46c94e586683361f242da214098999a62a989006
Bui Duc Thang <thangbd@vp9.vn>
2017-07-20 00:03:04 +0700

    update init cate

---
+++ ad/views.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+
+from elasticsearch import Elasticsearch
+from django.http import *
+
+import json
+
+def Get(request):
+    es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
+
+    #Search by userid
+    search = es.search(index="ad",body={"query": {"match": {'userid':'13'}}})
+    #print search
+
+    #Get by id
+    get = es.get(index="ad", doc_type="ad-net", id="AV1Wa8w4CM-GrZ83-11I")
+    eventId = get['_id']
+    source = get['_source']
+    print "event id:" , get['_id']
+    print "source: ", type(source)
+
+    #UPdate by id
+    #es.index(index="ad", doc_type="ad-net", id="AV1Wa8w4CM-GrZ83-11I", body={"userid":13,"quan":1})
+    
+    #Create
+    #es.index(index="ad", doc_type="ad-net", body={"userid":15,"dong ho":2})
+    
+    #Delete by Id
+    #es.delete(index="ad", doc_type="ad-net", id="AV1Y3Q6KxaR1NsDMNVAf")
+
+    return render(request,"ad.html")
+
+def AjaxRequest(request):
+    if(request.is_ajax() and request.POST):
+        ajaxData = request.POST.get('ajaxdata')
+        print ajaxData
+    return HttpResponse(json.dumps(ajaxData), content_type='application/json')
---
+++ ecommerce/views.py
@@ -0,0 +1,92 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+from elasticsearch import Elasticsearch
+from django.http import *
+
+import json
+
+def SignUp(request):
+    if(request.method == 'POST'):
+        username = request.POST['acc']
+        password = request.POST['password']
+        print username, ' ' , password
+
+        es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
+        
+        #Search username
+        search = es.search(index="ad",body={"query": {"match": {'username':username}}})
+        print search['hits']['total']
+        exist = search['hits']['total']
+
+        if(exist == 0):
+            #Create user
+            user = es.index(index="ad", doc_type="ad-net", body={"username":username,"password":password})
+            userid = user['_id']
+            print "userid: ", user['_id']
+            #Init categories
+            categories_goods = {}
+            categories_goods['ao'] = 0
+            categories_goods['quan'] = 0
+            categories_goods['vay'] = 0
+
+            print categories_goods
+
+            #Init Category of goods
+            data = {"userid":userid, "ao": categories_goods['ao'], "quan":categories_goods["quan"], "vay":categories_goods['vay']}
+            print data
+            es.index(index="ad", doc_type="cate", body=data)
+        else:
+            return HttpResponse("user name is existed")
+
+    return render(request, "ecommerce.html")
+
+def SignIn(request):
+    if(request.method == 'POST'):
+        username = request.POST['acc']
+        password = request.POST['password']
+        print username, ' ' , password
+
+        es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
+        
+        #Search user
+        search = es.search(index="ad",body={"query": {"match": {'username':username}}})
+        user = search['hits']['hits'][0]['_source']['username']
+        passs = search['hits']['hits'][0]['_source']['password']
+        userid = search['hits']['hits'][0]['_id']
+        print username, ' ' , passs
+        print userid
+
+        if(user == username and passs == password):
+            request.session['userid'] = userid
+            print request.session['userid']
+            # request.session['username'] = user
+            # print request.session['username']
+            #return HttpResponse('sign in success with userid: '+ userid + ' of username '+ user)
+            return HttpResponseRedirect('/ecommerce/goods')
+        else:
+            return HttpResponse("username or pass is wrong")
+
+    return render(request, "ecommerce.html")
+
+def Goods(request):
+    return render(request,"goods.html")
+
+def Detail(request):
+    es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
+
+    #Check category is exist
+    #search_cate = es.search(index = "ad", body={"querry":{"match":{'userid':request.session.get('userid')}}})
+    #exist = search_cate['hits']['total']
+    #print search_cate
+    #if(search_cate == 0):
+        #print "cate"
+        #Init Category of goods
+    
+
+    if(request.method == 'GET' and request.is_ajax()):
+        #Get category og goods
+        goods = request.GET.get('goods')
+        print "goods: " , goods
+        return HttpResponse(goods)
