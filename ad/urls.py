3cdf497ac178d12d66c2c4f0606f8a425a73c9e2
Datnv9 <thiet_thuc@ymail.com>
2017-07-27 16:12:42 +0700

    add display 2

--- ad/urls.py
+++ ad/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     url(r'^$', views.Get, name="get"),
     url(r'^ajax/', views.AjaxRequest, name="ajax"),
+    url(r'^profile/', views.Profile, name="profile"),
 ]
