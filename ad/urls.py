1ecf5e81bf0a97bee26099794a7a1645da74634c
datnv9 <thiet_thuc@ymail.com>
2017-07-27 17:37:27 +0700

    update

--- ad/urls.py
+++ ad/urls.py
@@ -3,7 +3,8 @@ from django.conf.urls import url
 from . import views 
 
 urlpatterns = [
-    url(r'^$', views.Get, name="get"),
+    url(r'^', views.Get, name="get"),
     url(r'^ajax/', views.AjaxRequest, name="ajax"),
     url(r'^profile/', views.Profile, name="profile"),
+    url(r'^create-profile/', views.Create_Profile, name="create_profile"),
 ]
--- ad/views.py
+++ ad/views.py
@@ -10,26 +10,20 @@ import json
 import ast
 
 from adnet import Service
+from django.shortcuts import redirect
 
 def Get(request):
     es = Elasticsearch([{'host': '10.12.11.161', 'port': 9200}])
 
     ip_mac = Service.Get_IP_MAC(request)
-    ip = ip_mac[1]
-    mac = ip_mac[0]
-    print 'mac: ',mac
-    
+    ip = ip_mac[0]
+    mac = ip_mac[1]
+
+    # ads = Service.Get_Ads_By_MacAdd(mac)
     #Search by userid
-    search = es.search(index="ad",body={"query": {"match": {'userid':'13'}}})
     #print search
-    
-    imgSrc = request.COOKIES.get('img_src','null')
-    productName = request.COOKIES.get('product_name','Chua co quang cao')
-    url = request.COOKIES.get('url','Chua co quang cao')
-    
+    #if (ads == 0):    
     # ads = zip('Chua co quang cao','#')
-    print 'productName: ', imgSrc
-    if(productName != 'Chua co quang cao'):
         # list_imgSrc = ast.literal_eval(imgSrc)
         # list_productName = ast.literal_eval(productName)
 
@@ -42,16 +36,20 @@ def Get(request):
         
         # print "after: ", list_productName
         # ads = zip(list_productName,list_imgSrc)
-
-        mac_add = Service.Get_IP_MAC()
-        print "mac_Add: ", mac_add[1]
-        return render(request,"display.html",{'mac':mac,'ip':ip,'imgSrc':imgSrc, 'productName':productName, 'url': url})
+        #print "mac_Add:", mac
+    imgSrc = Service.Read_File('/home/thuynv/Desktop/check_new.txt')[0]
+        #print "imgSrc:", imgSrc
+    return render(request,"display.html",{'mac':mac,'ip':ip,'imgSrc':imgSrc, 'productName':'Chua co quang cao', 'url': 'url'})
     #Get by id
     #get = es.get(index="ad", doc_type="ad-net", id="AV1Wa8w4CM-GrZ83-11I")
     #eventId = get['_id']
     #source = get['_source']
     #print "event id:" , get['_id']
     #print "source: ", type(source)
+    # imgSrc = ads['img']
+    # productName = ads['title']
+    # url = ads['url']
+    # cate = ads['cate']
 
     #UPdate by id
     #es.index(index="ad", doc_type="ad-net", id="AV1Wa8w4CM-GrZ83-11I", body={"userid":13,"quan":1})
@@ -61,9 +59,7 @@ def Get(request):
     
     #Delete by Id
     #es.delete(index="ad", doc_type="ad-net", id="AV1Y3Q6KxaR1NsDMNVAf")
-
-    
-    return render(request,"display.html",{'mac':mac,'ip':ip,'imgSrc':imgSrc, 'productName':productName, 'url': url})
+    # return render(request,"display.html",{'mac':mac,'ip':ip,'imgSrc':imgSrc, 'productName':productName, 'url': url})
 
 def AjaxRequest(request):
     if(request.is_ajax() and request.POST):
@@ -83,3 +79,15 @@ def Profile(request):
     url = ads['url']
     cate = ads['cate']
     return render(request,"display2.html",{'mac':mac,'ip':ip,'imgSrc':imgSrc, 'productName':productName, 'url': url})
+
+def Create_Profile(request):
+    if(request.method == 'POST'):
+        print "POST"
+        img_link = request.POST['link-img']
+        mac = request.POST['mac-id']
+
+        print "img_link:",img_link
+        print "mac:",mac
+
+        Service.Create_User("username","password",mac,img_link)
+        return redirect('profile')
